db77e0c87d54b82e79790914e9e26e5b50d69ce9
Lucas Uyezu <lucas.uyezu@gmail.com>
2009-05-03 19:13:36 -0300

    Aceito

--- done/calcula/calcula.rb
+++ done/calcula/calcula.rb
@@ -1,14 +1,5 @@
-def soma(t, s)
-  if t =~ /\+|\-/
-    $`.to_i * s + soma($', $& == '-' ? -1 : 1)
-  else
-    t.to_i * s 
-  end
-end
-
 n = 1
-r = Hash.new
-while($_ = gets.to_i) != 0
-  print "Teste #{n}\n#{soma(gets, 1)}\n\n"
+while gets.to_i != 0
+  print "Teste #{n}\n#{instance_eval gets}\n\n"
   n += 1
 end
