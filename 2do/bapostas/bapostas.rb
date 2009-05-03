df6f4f84638338274d44d8647a283cb6fe90188f
Lucas Uyezu <lucas.uyezu@gmail.com>
2009-05-03 19:07:17 -0300

    TLE

--- 2do/bapostas/bapostas.rb
+++ 2do/bapostas/bapostas.rb
@@ -1,24 +1,41 @@
 class Bapostas
   
-  def self.solve n
-    m = 0
-    a = Array.new n
-    while m < n
-      gets.split.each { |e| a[m] = e.to_i; m += 1 }
+  def initialize
+    @line = []
+    @index = 0
+  end
+  
+  def read_integer
+    if @index == @line.size
+      onon = gets
+      @line = onon.split
+      @index = 0
+      return read_integer
     end
+    result = @line[@index].to_i
+    @index += 1
+    return result
+  end
+  
+  def solve n
+    a = Array.new n
+    a.collect! { read_integer }
     result = max_ending_here = 0
-    for i in 0...a.size
-      max_ending_here += a[i]
-      max_ending_here = 0 if max_ending_here < 0
-      result = max_ending_here if result < max_ending_here
+    a.each do |e|
+      max_ending_here += e
+      max_ending_here = max_ending_here > 0 ? max_ending_here : 0
+      result = result > max_ending_here ? result : max_ending_here
     end
     result > 0 ? "The maximum winning streak is #{result}.\n" : "Losing streak.\n"
   end
   
 end
 
+bapostas = Bapostas.new
+n = bapostas.read_integer
 output = ""
-while (n = gets.to_i) != 0
-  output << Bapostas.solve(n)
+while n != 0
+  output << bapostas.solve(n)
+  n = bapostas.read_integer
 end
 print output
