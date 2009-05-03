b7e21cb6db6e731a4b0ca12af6ad56031e0a8d38
Lucas Uyezu <lucas.uyezu@gmail.com>
2009-05-03 19:06:25 -0300

    RE

--- 2do/bapostas/bapostas.rb
+++ 2do/bapostas/bapostas.rb
@@ -7,16 +7,18 @@ class Bapostas
       gets.split.each { |e| a[m] = e.to_i; m += 1 }
     end
     result = max_ending_here = 0
-    a.each do |e|
-      max_ending_here += e
+    for i in 0...a.size
+      max_ending_here += a[i]
       max_ending_here = 0 if max_ending_here < 0
       result = max_ending_here if result < max_ending_here
     end
-    puts result > 0 ? "The maximum winning streak is #{result}." : "Losing streak."
+    result > 0 ? "The maximum winning streak is #{result}.\n" : "Losing streak.\n"
   end
   
 end
 
+output = ""
 while (n = gets.to_i) != 0
-  Bapostas.solve n
+  output << Bapostas.solve(n)
 end
+print output
